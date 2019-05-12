9139e94f41e468132639a89dff80ea876a6dfdbc
Riceball LEE <snowyu.lee@gmail.com>
2019-05-12 14:10:36 +0800

    improvement(svg-textwrap): add textMetrics function

--- src/utils/svg-textwrap/textWidth.ts
+++ src/utils/svg-textwrap/textWidth.ts
@@ -1,12 +1,4 @@
-/**
- * Strips HTML and "un-escapes" escape characters.
- * @param {String} input
- */
-function htmlDecode(input) {
-  if (input === ' ') return input
-  const doc = new DOMParser().parseFromString(input.replace(/<[^>]+>/g, ''), 'text/html')
-  return doc.documentElement.textContent
-}
+import { default as textMetrics } from './textMetrics'
 
 /**
  *  @function textWidth
@@ -15,31 +7,7 @@ function htmlDecode(input) {
  *  @param {Object} [style] An object of CSS font styles to apply. Accepts any of the valid [CSS font property](http://www.w3schools.com/cssref/pr_font_font.asp) values.
  */
 export default function(text: string | string[], style?) {
-  style = Object.assign(
-    {
-      'font-size': 10,
-      'font-family': 'sans-serif',
-      'font-style': 'normal',
-      'font-weight': 400,
-      'font-variant': 'normal'
-    },
-    style
-  )
-
-  const context = document.createElement('canvas').getContext('2d') as CanvasRenderingContext2D
-
-  const font: any[] = []
-  font.push(style['font-style'])
-  font.push(style['font-variant'])
-  font.push(style['font-weight'])
-  font.push(typeof style['font-size'] === 'string' ? style['font-size'] : `${style['font-size']}px`)
-  // let s = `${style["font-size"]}px`;
-  // if ("line-height" in style) s += `/${style["line-height"]}px`;
-  // font.push(s);
-  font.push(style['font-family'])
-
-  context.font = font.join(' ')
-
-  if (text instanceof Array) return text.map(t => context.measureText(htmlDecode(t)).width)
-  return context.measureText(htmlDecode(text)).width
+  const metrics = textMetrics(text, style)
+  if (metrics instanceof Array) return metrics.map(t => t.width)
+  return metrics.width
 }
