cb075485602badebe1097e5ff58104c7c1d26c18
Riceball LEE <snowyu.lee@gmail.com>
2019-05-14 09:51:09 +0800

    feat(utils): add the hypenate func to convert camelCase string to dash

---
+++ src/utils/hyphenate.ts
@@ -0,0 +1,7 @@
+/**
+ * Hyphenate a camelCase string.
+ */
+const hyphenateRE = /\B([A-Z])/g
+export function hyphenate(str: string): string {
+  return str.replace(hyphenateRE, '-$1').toLowerCase()
+}
