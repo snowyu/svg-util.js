ce67c97be73773e5435e07ba514f9aa57c8a7a4e
Riceball LEE <snowyu.lee@gmail.com>
2020-04-16 16:37:31 +0800

    feat: add optional attrs to loadImage and getImageSize funcs

--- src/utils/get-image-size.ts
+++ src/utils/get-image-size.ts
@@ -1,8 +1,8 @@
 import { memoize } from 'lodash'
 import { loadImage } from './load-image'
 
-export const getImageSize = memoize(async function(aUrl: string) {
-  const vImg = await loadImage(aUrl)
+export const getImageSize = memoize(async function(aUrl: string, attrs?) {
+  const vImg = await loadImage(aUrl, attrs)
   const { width, height } = vImg
   return { width, height }
 })
--- src/utils/load-image.ts
+++ src/utils/load-image.ts
@@ -1,6 +1,14 @@
-export async function loadImage(url: string): Promise<HTMLImageElement> {
+import { setAttributes } from './set-attributes'
+
+interface IAttrs {
+  [k: string]: any
+}
+export async function loadImage(url: string, attrs?: IAttrs): Promise<HTMLImageElement> {
   return new Promise(function(resolve, reject) {
     const image = new Image()
+    if (attrs) {
+      setAttributes(image, attrs)
+    }
     image.onerror = reject
     image.onload = function() {
       resolve(image)
